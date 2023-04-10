cc8464089c526de81d27ec94231ad3aa5d11e491
jovanagjorgjieska <jovana.gjorgjieska@students.finki.ukim.mk>
2023-04-10 16:24:09 +0200

    [T1-18] - Create the project for the backend part

---
+++ api/src/main/java/mk/ukim/finki/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
