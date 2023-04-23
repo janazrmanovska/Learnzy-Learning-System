30208efc56d5f0230e03fd55893ce99b32506815
jovanagjorgjieska <jovana.gjorgjieska@students.finki.ukim.mk>
2023-04-23 18:06:02 +0200

    delete not needed files

--- api/src/main/java/mk/ukim/finki/api/model/User.java
+++ api/src/main/java/mk/ukim/finki/api/model/User.java
@@ -1,64 +1,42 @@
 package mk.ukim.finki.api.model;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-@Builder
 @Table(name="_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
-
-    private String firstName;
-    private String lastName;
+    private String username;
     private String email;
     private String password;
 
-    @Enumerated
-    private Role role;
+    @OneToMany(mappedBy = "user")
+    Set<QuizScore> scores;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-
+        return null;
     }
 
     @Override
     public String getPassword() {
-        return password;
-
+        return null;
     }
 
     @Override
     public String getUsername() {
-        return email;
-
+        return username;
     }
 
     @Override
--- api/src/main/java/mk/ukim/finki/api/service/LessonService.java
+++ api/src/main/java/mk/ukim/finki/api/service/LessonService.java
@@ -3,6 +3,7 @@ package mk.ukim.finki.api.service;
 import mk.ukim.finki.api.model.Category;
 import mk.ukim.finki.api.model.Lesson;
 import mk.ukim.finki.api.model.Level;
+import mk.ukim.finki.api.model.Quiz;
 
 import java.util.List;
 
@@ -10,8 +11,10 @@ public interface LessonService {
     List<Lesson> findAll();
     Lesson findById(Long id);
     void deleteById(Long id);
-    Lesson create(String title, String description, Category category, Level level, String urlPhoto, String urlVideo);
-    Lesson update(Long id, String title, String description, Long category, Level level, String urlPhoto, String urlVideo);
+    Lesson create(String title, String description, Category category,
+                  Level level, String urlPhoto, String urlVideo, Quiz quiz);
+    Lesson update(Long id, String title, String description, Long category,
+                  Level level, String urlPhoto, String urlVideo, Quiz quiz);
     List<Lesson> filterLevel(Level level);
     List<Lesson> filterCategory(Long categoryId);
 }
